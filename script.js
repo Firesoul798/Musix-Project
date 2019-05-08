fbb5ce9d159f0eecc9b12ca0c120d2c12e1a8c0d
Rahmel DeSoto <greysoul798@gmail.com>
2019-05-08 22:02:21 +0000

    clicker

---
+++ script.js
@@ -0,0 +1,8 @@
+/* global $ */
+$(document).ready(function() {
+    $("#clicker").click(function(){
+        var result= $("#songInput").val();
+        console.log(result);
+        
+    });
+}); 
