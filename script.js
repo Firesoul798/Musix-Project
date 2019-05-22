e1e5ba5b2ce188381bffcd97d0e2103d139996a5
Rahmel DeSoto <greysoul798@gmail.com>
2019-05-22 21:54:03 +0000

    musix day 4

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 /* global $ */
+ 
 $(document).ready(function() {
+    
+   
     $("#clicker").click(function(){
-        var result= $("#songInput").val();
+         
+         var result= $("#songInput").val();
+   
+    var urlvar = "https://api.soundcloud.com/tracks?q=" + result + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";    
         console.log(result);
-        
-    });
+$.ajax({
+    url:urlvar,
+    method:"GET",
+    success:function(response){
+        console.log(urlvar);
+        console.log(response[0].id);
+        console.log(response[0].duration);
+        console.log(response[0].uri);
+        console.log(response[0]["artwork_url"]);
+    }
+});
+});
 }); 
